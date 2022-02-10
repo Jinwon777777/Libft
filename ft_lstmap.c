02dce6f4ddd95d393608fd350a3d74754d8f4422
Jinwon777777 <wlsdnjs273@gmail.com>
2022-02-10 17:45:56 +0900

    17:45

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jiha <jiha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 20:17:01 by jiha              #+#    #+#             */
-/*   Updated: 2022/02/10 17:40:22 by jiha             ###   ########.fr       */
+/*   Updated: 2022/02/10 17:45:14 by jiha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,10 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*tmp;
 	t_list	*ans;
-	t_list	*now;
 
 	if (!(lst) || !(f) || !(del))
 		return (NULL);
-	ans = ft_lstnew(f(lst->content));
-	if (!ans)
-		return (NULL);
-	now = ans;
-	lst = lst->next;
+	ans = NULL;
 	while (lst)
 	{
 		tmp = ft_lstnew(f(lst->content));
@@ -33,8 +28,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			ft_lstclear(&ans, del);
 			return (NULL);
 		}
-		now->next = tmp;
-		now = tmp;
+		ft_lstadd_back(&ans, tmp);
 		lst = lst->next;
 	}
 	return (ans);
