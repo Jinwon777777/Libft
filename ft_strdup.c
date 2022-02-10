8912dbcb0091c48ad05d80200897e9460a11b4a4
Jinwon777777 <wlsdnjs273@gmail.com>
2022-02-10 10:55:34 +0900

    10:55

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jiha <jiha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 16:49:47 by jiha              #+#    #+#             */
-/*   Updated: 2022/02/10 10:42:27 by jiha             ###   ########.fr       */
+/*   Updated: 2022/02/10 10:52:58 by jiha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strdup(const char *s1)
 	char	*tmp;
 
 	i = ft_strlen(s1);
-	tmp = (char *)malloc(sizeof(char));
+	tmp = (char *)malloc(sizeof(char) * (i + 1));
 	if (!(tmp))
 		return (NULL);
 	ft_strlcpy(tmp, s1, i + 1);
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jiha <jiha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 13:23:12 by jiha              #+#    #+#             */
-/*   Updated: 2022/02/10 10:23:56 by jiha             ###   ########.fr       */
+/*   Updated: 2022/02/10 10:55:19 by jiha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ size_t	ft_strlcat(char	*dst, const char *src, size_t dstsize)
 	i = 0;
 	dl = ft_strlen(dst);
 	sl = ft_strlen(src);
-	if (dstsize - 1 < dl)
+	if (dstsize < dl + 1)
 		return (sl + dstsize);
-	if (dstsize - 1 > dl)
+	if (dstsize > dl + 1)
 	{
 		while ((dl + i < dstsize - 1) && (i < sl))
 		{
