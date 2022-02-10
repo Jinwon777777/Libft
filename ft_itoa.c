33aa79f1bff421688fb1bf02f32245115f91693b
Jinwon777777 <wlsdnjs273@gmail.com>
2022-02-10 16:50:54 +0900

    16:50

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jiha <jiha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 13:16:27 by jiha              #+#    #+#             */
-/*   Updated: 2022/02/06 15:06:11 by jiha             ###   ########.fr       */
+/*   Updated: 2022/02/10 16:50:36 by jiha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static int	ft_len(int n)
 	i = 0;
 	if (n < 0)
 		i++;
-	while (i != 0)
+	while (n != 0)
 	{
 		n = n / 10;
 		i++;
@@ -29,25 +29,24 @@ static int	ft_len(int n)
 
 char	*ft_itoa(int n)
 {
-	int		len;
-	char	*arr;
-	long	tmp;
+	int			len;
+	char		*arr;
+	long long	tmp;
 
 	len = ft_len(n);
+	tmp = n;
 	arr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!(arr))
 		return (NULL);
-	if (n < 0)
+	if (tmp < 0)
 	{
 		arr[0] = '-';
-		tmp = (long)(-1 * n);
+		tmp = (-1 * tmp);
 	}
-	else
-		tmp = (long)n;
 	arr[len--] = '\0';
-	while (tmp != 0)
+	while (len != 0)
 	{
-		arr[len] = (tmp % 10);
+		arr[len] = (tmp % 10) + '0';
 		tmp = (tmp / 10);
 		len--;
 	}
