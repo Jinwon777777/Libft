fbf98a64f18b9880fdbb0a211d0ab3f1ae0578f1
Jinwon777777 <wlsdnjs273@gmail.com>
2022-02-11 11:05:45 +0900

    11:05

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jiha <jiha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 14:16:50 by jiha              #+#    #+#             */
-/*   Updated: 2022/02/11 10:59:38 by jiha             ###   ########.fr       */
+/*   Updated: 2022/02/11 11:05:18 by jiha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	char	*s1;
+	int		i;
 
+	s1 = (char *)s;
 	i = ft_strlen(s);
 	while (i > 0)
 	{
-		if (s[i] == (char)c)
-			return (s + i);
+		if (s1[i] == (char)c)
+			return (s1 + i);
 		i--;
 	}
-	if (s[i] == (char)c)
-		return (s);
+	if (s1[i] == (char)c)
+		return (s1);
 	return (NULL);
 }
